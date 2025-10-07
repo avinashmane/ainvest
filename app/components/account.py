413c357b8df49a93d705657973e104fff4682dcb
Avinash Mane <avinashmane@yahoo.com>
2025-10-07 10:56:39 +0530

    leaderboard alignment

--- app/components/account.py
+++ app/components/account.py
@@ -7,11 +7,12 @@ def show_portfolio(df):
     df=df.sort_values('total',ascending=False).reset_index(drop=True).fillna('')
     
     for i,x in df.iterrows():
-        with st.container(horizontal=True):
+        with st.container(horizontal=True,):
             st.header(f"{i+1}")
-            st.write(dedent(f"""
+            with st.container(width=250):
+                st.write(dedent(f"""
+                    ## :yellow[{curr(x.total,0)}]
                     #### {x.id} 
-                    #### :yellow[{curr(x.total,0)}]
                     """))
             st.write(dedent(f"""
                     | Total| {curr(x.total)} |
